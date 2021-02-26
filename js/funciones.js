3879d9cb4716b65c827480528e78082f7612618d
Juan Cruz Campassi <8306juan@gmail.com>
2021-02-26 17:43:39 -0300

    Mas info + Easter Egg

--- js/funciones.js
+++ js/funciones.js
@@ -6,7 +6,6 @@ let sendFC = document.querySelector(".formContacto");
 let closeCon = document.getElementById('closeContact');
 
 
-let btnRes = document.querySelector(".btnRes");
 let nav = document.querySelector(".nav");
 
 let activeScroll=true;
@@ -24,10 +23,60 @@ for (let i=0; i<navLis.length; i++){
 	})
 }
 
+/*
+	Cosas que Amo	Cosas que Amo	Cosas que Amo	Cosas que Amo	Cosas que Amo
+*/
+let fotoP = document.getElementById('foto');
+let cqa1 = document.querySelector('#cqa1');
+let cqa2 = document.querySelector('#cqa2');
+let cqa3 = document.querySelector('#cqa3');
 
-window.addEventListener("scroll", ()=>{
+const cosasQueAmo = ()=>{
+	if (cqa1.classList.value == 'cqa1off'){
+
+		cqa1.classList.remove('cqa1off');
+		cqa1.classList.add('cqa1on');
+
+		cqa2.classList.remove('cqa2off');
+		cqa2.classList.add('cqa2on');
 		
+		cqa3.classList.remove('cqa3off');
+		cqa3.classList.add('cqa3on');
+	}else if(cqa1.classList.value == 'cqa1on'){
+
+		cqa1.classList.remove('cqa1on');
+		cqa1.classList.add('cqa1off');
+		
+		cqa2.classList.remove('cqa2on');
+		cqa2.classList.add('cqa2off');
+		
+		cqa3.classList.remove('cqa3on');
+		cqa3.classList.add('cqa3off');
+	}
+}
+
+fotoP.addEventListener('mouseover', cosasQueAmo);
+fotoP.addEventListener('mouseout', cosasQueAmo);
+
+
+cqa2.lastElementChild.addEventListener('dblclick',()=>{
+	alert(`:O \n ¡¡GENIAL!!\n Parece que descubriste un Easter Egg`);
+
+	let easterEggs = document.querySelector('.easterEggs');
+
+	easterEggs.style.transition="1s";
+	easterEggs.style.opacity=1;
+	easterEggs.innerHTML += `
+	<div style="max-width: 100px; text-align: center;">
+		<img src="img/easterEgg.jpg">
+		<p>Shadow of the Colossus</p>		
+	</div>`;
 })
+
+
+/*
+	CONTACTO	CONTACTO	CONTACTO	CONTACTO	CONTACTO	CONTACTO
+*/
 
 liC.addEventListener("click",()=>{
 	sendFC.style.top=`${scrollY}px`;
@@ -39,14 +88,3 @@ closeCon.addEventListener("click",()=>{
 	sendFC.classList.toggle("on");
 	document.body.style.overflowY = 'initial';
 })
-
-btnRes.addEventListener("click", ()=>{
-	
-	if (nav.style.display=="block") {
-		nav.style.display="none";
-	}else{
-		nav.style.display="block";
-	}
-	
-})
-
